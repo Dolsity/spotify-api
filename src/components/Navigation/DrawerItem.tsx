ed37abb99e481182b45953ce8631f0862f87a15a
Jay <Acorder43812@gmail.com>
2025-06-26 00:46:30 -0700

    Reformat Navigation components

--- src/components/Navigation/DrawerItem.tsx
+++ src/components/Navigation/DrawerItem.tsx
@@ -5,28 +5,28 @@ import { useNavigate } from 'react-router-dom'
 import { ListItemButton, ListItemText } from '@mui/material'
 
 const NavItem = ({
-	setOpen,
-	item,
+  setOpen,
+  item,
 }: {
-	setOpen: Dispatch<SetStateAction<boolean>>
-	item: iDrawerItem
+  setOpen: Dispatch<SetStateAction<boolean>>
+  item: iDrawerItem
 }) => {
-	const navigate = useNavigate()
+  const navigate = useNavigate()
 
-	const redirect = (url: string) => {
-		navigate(url)
-		setOpen(false)
-	}
+  const redirect = (url: string) => {
+    navigate(url)
+    setOpen(false)
+  }
 
-	return item.condition() ? (
-		<React.Fragment key={item.id}>
-			<ListItemButton onClick={() => redirect(item.url)}>
-				<ListItemText primary={item.title} />
-			</ListItemButton>
-		</React.Fragment>
-	) : (
-		<></>
-	)
+  return item.condition() ? (
+    <React.Fragment key={item.id}>
+      <ListItemButton onClick={() => redirect(item.url)}>
+        <ListItemText primary={item.title} />
+      </ListItemButton>
+    </React.Fragment>
+  ) : (
+    <></>
+  )
 }
 
 export default NavItem
--- src/components/Navigation/Navigator.tsx
+++ src/components/Navigation/Navigator.tsx
@@ -11,99 +11,99 @@ import CloseIcon from '@mui/icons-material/Close'
 import DrawerItem from './DrawerItem'
 
 const Navigator = () => {
-	const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false)
 
-	const data: iDrawerItem[] = [
-		{
-			id: 'nav-home',
-			title: 'Home',
-			url: '/',
-			icon: undefined,
-			condition: () => true,
-		},
-		{
-			id: 'nav-login',
-			title: 'Login',
-			url: '#',
-			icon: undefined,
-			condition: () => true,
-		},
-	]
+  const data: iDrawerItem[] = [
+    {
+      id: 'nav-home',
+      title: 'Home',
+      url: '/',
+      icon: undefined,
+      condition: () => true,
+    },
+    {
+      id: 'nav-login',
+      title: 'Login',
+      url: '#',
+      icon: undefined,
+      condition: () => true,
+    },
+  ]
 
-	return (
-		<>
-			<AppBar sx={{ bgcolor: 'primary.main' }} position="relative">
-				<Toolbar>
-					<Typography variant="h5" textTransform={'uppercase'}>
-						spotify stats
-					</Typography>
-					<IconButton
-						onClick={() => setOpen(true)}
-						size="large"
-						edge="end"
-						aria-label="menu"
-						sx={{ ml: 'auto' }}
-					>
-						<Tooltip title="Open Menu" placement="left">
-							{/* TODO: Default avatar if not authenticated, else user avatar */}
-							<Avatar
-								sx={{
-									width: 38,
-									height: 38,
-									border: '1.5px solid #25b359', // TODO: Green border if authenticated, else default
-								}}
-							></Avatar>
-						</Tooltip>
-					</IconButton>
-				</Toolbar>
-			</AppBar>
+  return (
+    <>
+      <AppBar sx={{ bgcolor: 'primary.main' }} position="relative">
+        <Toolbar>
+          <Typography variant="h5" textTransform={'uppercase'}>
+            spotify stats
+          </Typography>
+          <IconButton
+            onClick={() => setOpen(true)}
+            size="large"
+            edge="end"
+            aria-label="menu"
+            sx={{ ml: 'auto' }}
+          >
+            <Tooltip title="Open Menu" placement="left">
+              {/* TODO: Default avatar if not authenticated, else user avatar */}
+              <Avatar
+                sx={{
+                  width: 38,
+                  height: 38,
+                  border: '1.5px solid #25b359', // TODO: Green border if authenticated, else default
+                }}
+              ></Avatar>
+            </Tooltip>
+          </IconButton>
+        </Toolbar>
+      </AppBar>
 
-			<SwipeableDrawer
-				anchor="right"
-				open={open}
-				onOpen={() => setOpen(true)}
-				onClose={() => setOpen(false)}
-			>
-				<Box sx={{ width: 250 }} role="presentation">
-					<List
-						subheader={
-							<Box
-								sx={{
-									display: 'flex',
-									alignItems: 'center',
-									justifyContent: 'space-between',
-									px: 3,
-									py: 1,
-								}}
-							>
-								{/* TODO: Hello <user> if authenticated, else "spotify stats" */}
-								<Typography variant="h6" textTransform={'uppercase'}>
-									spotify stats
-								</Typography>
-								<IconButton
-									onClick={() => setOpen(false)}
-									size="large"
-									edge="end"
-									color="inherit"
-									aria-label="close menu"
-									sx={{ ml: 'auto' }}
-								>
-									<Tooltip title="Close Menu" placement="bottom-end">
-										<CloseIcon />
-									</Tooltip>
-								</IconButton>
-							</Box>
-						}
-					>
-						<Divider />
-						{data.map((el) => (
-							<DrawerItem key={el.id} setOpen={setOpen} item={el} />
-						))}
-					</List>
-				</Box>
-			</SwipeableDrawer>
-		</>
-	)
+      <SwipeableDrawer
+        anchor="right"
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+      >
+        <Box sx={{ width: 250 }} role="presentation">
+          <List
+            subheader={
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
+                  px: 3,
+                  py: 1,
+                }}
+              >
+                {/* TODO: Hello <user> if authenticated, else "spotify stats" */}
+                <Typography variant="h6" textTransform={'uppercase'}>
+                  spotify stats
+                </Typography>
+                <IconButton
+                  onClick={() => setOpen(false)}
+                  size="large"
+                  edge="end"
+                  color="inherit"
+                  aria-label="close menu"
+                  sx={{ ml: 'auto' }}
+                >
+                  <Tooltip title="Close Menu" placement="bottom-end">
+                    <CloseIcon />
+                  </Tooltip>
+                </IconButton>
+              </Box>
+            }
+          >
+            <Divider />
+            {data.map((el) => (
+              <DrawerItem key={el.id} setOpen={setOpen} item={el} />
+            ))}
+          </List>
+        </Box>
+      </SwipeableDrawer>
+    </>
+  )
 }
 
 export default Navigator
