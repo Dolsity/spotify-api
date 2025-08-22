6687ffb2bda2885792ab36f268532e74fa2520a1
Jay <Acorder43812@gmail.com>
2025-08-22 14:35:55 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { CssBaseline, ThemeProvider, Box } from '@mui/material'
 
 import Navigator from './components/Navigation/Navigator'
 
 import Home from './pages/Home'
 import Artist from './pages/Artist'
 import TopArtists from './pages/TopArtists'
+import Track from './pages/Track'
 import Login from './pages/Login'
 import Logout from './pages/Logout'
 import NotFound from './pages/NotFound'
@@ -18,25 +19,32 @@ export function App() {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Navigator />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="top-artists">
-            <Route index element={<Navigate to="short-term" replace />} />
-            <Route path="short-term" element={<TopArtists />} />
-            <Route path="medium-term" element={<TopArtists />} />
-            <Route path="long-term" element={<TopArtists />} />
-            <Route path="*" element={<Navigate to="short-term" replace />} />
-          </Route>
-          <Route path="artist">
-            <Route index element={<Navigate to="" replace />} />
-            <Route path=":id" element={<Artist />} />
-          </Route>
-          <Route path="login" element={<Login />} />
-          <Route path="logout" element={<Logout />} />
-          <Route path="deauthorize" element={<Deauthorize />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Box minWidth={300}>
+          <Navigator />
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="top-artists">
+              <Route index element={<Navigate to="short-term" replace />} />
+              <Route path="short-term" element={<TopArtists />} />
+              <Route path="medium-term" element={<TopArtists />} />
+              <Route path="long-term" element={<TopArtists />} />
+              <Route path="*" element={<Navigate to="short-term" replace />} />
+            </Route>
+            <Route path="track">
+              <Route index element={<Navigate to="" replace />} />
+              <Route path=":id" element={<Track />} />
+            </Route>
+            <Route path="artist">
+              <Route index element={<Navigate to="" replace />} />
+              <Route path=":id" element={<Artist />} />
+            </Route>
+            <Route path="login" element={<Login />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="deauthorize" element={<Deauthorize />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Box>
       </ThemeProvider>
     </>
   )
