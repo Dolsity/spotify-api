12850fee45668c402a6f9ae7a97d9d0393f72788
Jay <Acorder43812@gmail.com>
2025-08-23 23:41:35 -0700

    Update types.d.ts

--- src/types.d.ts
+++ src/types.d.ts
@@ -5,3 +5,13 @@ interface iDrawerItem {
   url: string
   condition: () => boolean
 }
+
+interface iDrawerDropdown {
+  id: string
+  icon: JSX.Element
+  title: string
+  drop: number
+  items: iItem[]
+  condition: () => boolean
+}
+
